cb87e9768cf008c92b5906d1e480a34047919a26
Sam Ma <sam_newyork@163.com>
2018-05-31 23:27:22 +0800

    #update readme.md

--- closure/grammer/grammer.js
+++ closure/grammer/grammer.js
@@ -50,6 +50,90 @@ var status = flight.status || "unknown";
 console.log("status value: " + status);
 // 从undefined属性中进行读取property会返回TypeError异常类型.可以通过&&运算符来避免错误.
 console.log("flight.equipment:" + flight.equipment);    // 返回的值为undefined.
-console.log("flight.equipment.model:" + flight.equipment.model);    // 会抛出"TypeError异常类型"
+// console.log("flight.equipment.model:" + flight.equipment.model);    // 会抛出"TypeError异常类型"
 // 表达式计算之后的结果值为Undefined.
-console.log("&& operator避免throw异常类型: " + flight.equipment.model&&flight.equipment);
+// console.log("&& operator避免throw异常类型: " + flight.equipment.model && flight.equipment);
+
+// 4.对于属性的更新可以通过赋值语句来执行,如果属性名已经存在于对象里,那么这个属性的值就会被替换.
+stooge['first-name'] = 'Jerome';
+// 如果该属性在原有对象中并不存在,则该属性就会被添加到原有队形内部.
+stooge['middle-name'] = 'Lester';
+// 在js中对象通过引用来进行传递,它们永远都不会被赋值.
+var x = stooge;
+stooge.nickname = 'Curly';
+var nick = x.nickname;  // nick的值为Curly.
+// 另外一种类型,a、b、c 3个对象都指向相同的对象.
+var a = b = c = {};
+
+// 5.prototype原型属性(每个对象都有一个连接到原型对象),并且它可以从中继承属性.多有对象通过字面量创建的对象都连接到
+// Object.prototype,它是Javascript中的标配对象.给Object对象创建一个create()方法,该方法创建一个使用原型对象作为其原型的新对象.
+if(typeof Object.beget !== 'function') {
+    Object.create = function(o) {
+        var F = function() {};
+        F.prototype = o;
+        return new F();
+    };
+}
+var another_stooge = Object.create(stooge);
+// 原型链接在更新的时候是不起作用的,当我们对某个对象作出改变的时候,不会触及该对象的原型.
+another_stooge['first-name'] = 'Harry';
+another_stooge['middle-name'] = 'Moses';
+another_stooge.nickname = 'Moe';
+console.log('prototype type {stooge.nickname}: ' + stooge.nickname);
+// 当给stooge对象设置属性之后,在another_stooge可以通过原型链获取得到该属性.
+stooge.prefession = 'actor';
+console.log("another_stooge.prefession:" + another_stooge.prefession);
+
+// 6.js也通过类似的reflection机制检查对象的属性值(使用typeof关键字).
+typeof flight.number        // 'number'
+typeof flight.status        // 'string'
+typeof flight.arrival       // 'object'
+// 如果是原型链中的对象也会产生值.
+typeof flight.toString      // 'function'
+typeof flight.constructor   // 'function'
+// 如果执行检测对象自身而不去检测原型中的属性值,则可以使用hasOwnProperty方法.
+console.log('flight.number property: ' + flight.hasOwnProperty('number'));
+console.log('flight.constructor: ' + flight.hasOwnProperty('constructor'));
+// 如果要列出对象所有的属性则可以使用for.in来进行遍历;在使用for.in对对象属性进行遍历的时候,对象的属性是不确定的.
+// 如果要明确对象属性的值最好使用数组的形式进行表示,对象的属性作为数组的元素通过for循环从中取出元素.
+var name;
+for(name in another_stooge) {
+    if(typeof another_stooge[name] !== 'function') {
+        console.log(name + ":" + another_stooge[name]);
+    }
+}
+var i;
+var properties = ['first-name', 'middle-name', 'last-name', 'profession'];
+for(i = 0; i < properties.length; i++) {
+    console.log(properties[i] + ": " + another_stooge[properties[i]]);
+}
+// delete运算符可以删除对象的某属性,如果对象包含有该属性那么该属性就会被移除,它不会触及原型链中的任何对象
+// (如果对象的属性被删除,如果原型链中包含有同名属性,则会显示同名属性的值).
+delete another_stooge.nickname
+console.log('delete another_stooge.nickname: ' + another_stooge.nickname);
+
+/**
+ * 比较好的建议:减少全局变量污染(global abatement)
+ * javascript可以很随意地定义全局变量来容纳你的应用程序的所有资源,遗憾的是全局变量削弱了程序的灵活性,应避免使用。
+ * 只要把全局性的资源都纳入一个名称空间之下,你的应用程序与其他应用程序、组件和类库之间发生冲突的可能性就会显著降低。
+ * 你的应用程序变得更易阅读,因为很明显.我们会看到使用必报来进行信息隐藏的方式,是一种有效减少全局污染的方法.
+ * */
+var MYAPP = {};
+MYAPP.stooge = {
+    'first-mame': "Joe",
+    "last-name": "Howard"
+};
+MYAPP.flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+        IATA: "SYD",
+        time: "2004-09-22 14:55",
+        city: "Sydney"
+    },
+    arrival: {
+        IATA: "LAX",
+        time: "2004-09-22 10:42",
+        city: "los angeles"
+    }
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
 ### javascript_syntax基本语法(函数、原型继承、function)
-1. javascript基本数据类型: 数字、字符串、布尔值(true & false)、null值和undefined.其它所有的值都是对象，数字、字符串和布尔值貌似对象，因为它们拥有方法，但是它们是不可变的。javascript中的对象是可变的键控集合(keyed collections)。在javascript中，数组是对象，函数也是对象、正则表达式也是对象，当然，对象也是对象。 
+1. javascript基本数据类型: 数字、字符串、布尔值`(true & false)`、null值和undefined.其它所有的值都是对象，数字、字符串和布尔值貌似对象，因为它们拥有方法，但是它们是不可变的。javascript中的对象是可变的键控集合(keyed collections)。在javascript中，数组是对象，函数也是对象、正则表达式也是对象，当然，对象也是对象。 
+> * 对象字面量：对象字面量是一种非常方便地创建对象值得表示法，一个对象字面量就是包围在一堆花括号中的零个或者多个"名/值"对。
+```javascript
+var stooge = {
+  // 关于属性名的命名规范,如果属性名称是一个合法的js标识符,则可以进行直接的使用。
+  // 对于不合法的"-"符号则需要使用""双引号进行包含.
+  firstName : "Jerome",
+  "last-name": "Howard"
+};
+```
+> 关于对象属性的检索retrieval，可以直接使用object.property进行获取。如果属性名称符合变量的命名规范，则可以使用property直接可以使用.进行获取`如flight.department.IATA`；否则就需要使用`[]`进行包装进行提取`stooge["last-name"]`；
+如果要删除对象的某个属性值得时，可以直接使用delete操作符 `delete stooge.firstName`，删除之后若需要再重新去检索该属性，则会在对象的原型链中进行查询；
