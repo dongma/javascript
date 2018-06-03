0b28b77b18941bd3ab0c03976fcadf2e37f9bd41
Sam Ma <sam_newyork@163.com>
2018-06-03 11:54:37 +0800

    #function

--- closure/grammer/function.js
+++ closure/grammer/function.js
@@ -2,9 +2,8 @@
  * Created by dong on 18/6/1.
  */
 var value = 86;     // 定义全局的value变量.
-// 1.声明函数字面量与myobject对象.
+// 1.声明函数字面量与myobject对象(方法调用模式).
 var add = function(a, b) {
-    console.log("arguments: " + a + " > " + b);
     return a + b;
 };
 var myObject = {
@@ -20,14 +19,107 @@ myObject.increment(2);
 console.log("myObject.increment(2): " + myObject.value);
 var sum = add(3, 4);
 
-// 函数的调用模式(this绑定错误的时候),给myObject增加一个double方法.
+/*// 函数的调用模式(this绑定错误的时候),给myObject增加一个double方法.
 myObject.double = function() {
-   var helper = function() {
-       console.log("myObject.double method this.value: " + this.value);
+   // helper方法的this绑定被绑定到全局对象,使用该方式this绑定存在问题.
+   // 所以不能共享该方法对对象的访问权,在内部的定义方法中,this指向的是全局的Object对象,而不是外部方法中的this对象.
+    var helper = function() {
+       // myObject.double method this.value: undefined {this object}:[object global]
+       console.log("myObject.double method this.value: " + value + " {this object}:" + this);
        this.value = add(this.value, this.value);
    };
    helper();
+};*/
+
+myObject.double = function() {
+    var that = this;
+    var helper = function() {
+        console.log("global value: " + value + " {this object}:" + this + " {that object}:" + that);
+        that.value = add(that.value, that.value);
+    };
+    helper();
 };
-// 尝试以方法的形式调用double().
+
+// 2.另一中方式,函数调动模式(当一个函数并非一个对象的属性时,那么它就是被当做一个函数来调用的).
 myObject.double();
 console.log("myObject.value: " + myObject.value);
+
+// 3.构造器调用模式(the constructor invocation pattern)
+// 创建一个名为Quo的构造函数,它构造以个带有status属性的对象.
+var Quo = function (string) {
+    this.status = string;
+};
+// 给Quo的所有实例提供一个名为get_status的公共方法(通过原型Quo.prototype添加属性).
+Quo.prototype.get_status = function() {
+    return this.status;
+};
+// 构造一个Quo实例然后调用实例的方法myQuo.get_status().通过new操作符创建函数对象的时候,其
+// 在背后会创建一个连接到该函数prototype成员的新对象.同时this会被绑定到那个新的对象.
+var myQuo = new Quo("confused");
+console.log("myQuo.get_status: " + myQuo.get_status());
+
+// 4.apply调用模式(apply方法让我们构建一个参数数组传递给调用函数,它也允许我们选择this的值).
+// apply方法接收两个参数,第一个参数要绑定给this的值,第二个就是一个参数数组.
+var statusObject = {
+    status: 'A-OK'
+};
+var statusValue = Quo.prototype.get_status.apply(statusObject);
+console.log("statusObject.status: " + statusValue);
+
+// algorithm 汉诺塔问题(数学家的问题).
+/** 算法的核心:
+ *   1.首先其将较小的盘子移动到辅助圆盘上,从而露出下面较大的盘子.
+ *   2.移动下面的圆盘到目标柱子上.
+ *   3.最后将较小的盘子从从辅助柱子移动到目标柱子上.
+ *  hanoi(2, src=src, aux=aux, dst=dst) disc=3
+ *  hanoi(2, src=src, aux=dst, dst=aux) disc=2
+ *  hanoi(1, src=src, aux=aux, dst=dst) disc=1
+ */
+var hanoi = function(disc, src, aux, dst) {
+    if(disc > 0) {
+        hanoi(disc - 1, src, dst, aux);
+        console.log("move disc " + disc + " from " + src + " to " + dst);
+        hanoi(disc - 1, aux, src, dst);
+    }
+};
+hanoi(3, 'src', 'aux', 'dst');
+
+// 4.柯里化curry,扩充类型的功能.符合条件时才增加方法.通过给Function.prototype增加一个Method方法,
+// 我们下次给对象添加方法的时候就不必键入prototype这几个字符,缺省掉一些麻烦.
+Function.prototype.method = function(name, func) {
+    if(!this.prototype[name]) {
+        this.prototype[name] = func;
+    }
+    return this;
+};
+Function.method('curry', function() {
+    var slice = Array.prototype.slice,
+        args = slice.apply(arguments),
+        that = this;
+    return function() {
+      return that.apply(null, args.concat(slice.apply(arguments)));
+    };
+});
+var addCurryFunc = add.curry(1);
+// 在curry柯里化函数中多余的参数会被忽略.
+console.log("CurryFunc: " + addCurryFunc(2));
+
+/**
+ * 5.记忆(memoization).我们在一个名为memo的数组里面存放我们的存储结果,存储结果可以隐藏在闭包中.
+ * 当函数被调用的时候,先去检查结果是否存在,如果已经存在,就立即返回这个结果.
+ */
+var fibonacci = function() {
+   var memo = [0, 1];
+   var fib = function(n) {
+       var result = memo[n];
+       if(typeof result !== 'number') {
+           result = fib(n-1) + fib(n-2);
+           memo[n] = result;
+       }
+       return result;
+   };
+   return fib;
+}();
+for(var i = 0; i <= 10; i++) {
+    console.log("fibonacci[" + i + "] " + fibonacci(i));
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 ## javascript_syntax基本语法(函数、原型继承、function)
+1. javascript语言基本的数据类型以及对象字面量.
 > javascript基本数据类型: 数字、字符串、布尔值`(true & false)`、null值和undefined.其它所有的值都是对象，数字、字符串和布尔值貌似对象，因为它们拥有方法，但是它们是不可变的。javascript中的对象是可变的键控集合(keyed collections)。在javascript中，数组是对象，函数也是对象、正则表达式也是对象，当然，对象也是对象。 
 > 对象字面量：对象字面量是一种非常方便地创建对象值得表示法，一个对象字面量就是包围在一堆花括号中的零个或者多个"名/值"对。
 ```javascript
@@ -11,3 +12,14 @@ var stooge = {
 ```
 > 关于对象属性的检索retrieval，可以直接使用object.property进行获取。如果属性名称符合变量的命名规范，则可以使用property直接可以使用.进行获取`如flight.department.IATA`；否则就需要使用`[]`进行包装进行提取`stooge["last-name"]`；
 如果要删除对象的某个属性值得时，可以直接使用delete操作符 `delete stooge.firstName`，删除之后若需要再重新去检索该属性，则会在对象的原型链中进行查询；
+2. 关于javascript中的函数(function).
+>javascript中的函数就是对象，对象是“名/值”对的集合并且拥有一个连接到原型对象的隐藏连接。函数对象连接到`Function.prototype`(该原型对象本身连接到`Object.prototype`)。每个函数在创建的时候会附加两个隐藏属性:函数的上下文和实现该函数的代码；<br/>
+>javascript中函数的调用方式主要有4种：方法调用模式(函数作为对象的属性)、函数调用模式、构造器调用模式(使用new操作符)、apply调用模式。`()`充当函数调用运算符，在操作符中传入实际的参数。与强编译型语言不同的是，当调用javascript函数时，如果实际参数的数量与声明时的形参个数不同，多余的实参会被忽略不足个实参会使用undefined进行补充。不同的方法调用方式关于this绑定的形式：在方法中this绑定发生在方法被调用的时期；普通的函数调用时其`this`是默认绑定到全局对象，如果将函数声明在其它函数的内部，this被绑定了错误的值，所以不能共享该方法对对象的访问权；如果使用构造器调用方法时，js会在背后创建一个连接到该函数的prototype成员的新对象，同时this会被绑定到那个新对象上；apply方法允许我们创建一个参数数组传递给调用函数。它允许我们选择`this`值，第二个参数就是一个参数数组。
+```javascript
+/**
+ *statusObject并没有继承自Quo.prototype,但我们可以在statusObject上调用get_status方法，尽管statusObject并没有一个名为get_status的方法.
+ */
+var statusObject = {status: 'A-OK'};
+// 通过apply指定get_status方法调用的上下文为statusObject对象，因而其获取到的是statusObject的status属性值.
+var status = Quo.prototype.get_status.apply(statusObject);  // status的值为'A-OK'
+```
