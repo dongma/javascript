ea6e183b48e22ddd6d72b48c583531cd8a37884d
SamMa <sam_newyork@163.com>
2019-01-25 00:51:37 +0800

    Add mongodb aggregate operate

---
+++ aggregate/mono-aggregate.js
@@ -0,0 +1,24 @@
+/**
+ * 1.mongodb Aggregate pipeline
+ * */
+db.orders.aggregate([
+    {"$match": {status: "A"}},      // stage 1: $match stage
+    {"$group": {_id: "$cust_id", total:{$sum:"$amount"}}}   // stage2: $group stage
+]);
+
+// $addFields to add additional field, and using $size to calculate array.length
+db.getCollection('books').aggregate([
+    {$match: {_id: ObjectId("5c3cc6ce17ed628e68719072")}},
+    {$addFields: {length: {$size:'$books'}}},
+    // could not $project array object
+    {$project : {_id: 0, length: 1}}
+]);
+
+db.getCollection('books').aggregate([
+    {$sort: {_id: -1}},
+    // the books.length is 3 <match all elements>
+    {$match: {"books": {$size: 3}}},
+    // limit the result of total size
+    {$limit: 2}
+]);
+
