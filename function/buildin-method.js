9df3af87a52ba411f2c3c2d1ead6a382971632b1
SamMa <sam_newyork@163.com>
2019-04-09 00:54:27 +0800

    Array.reverse、Array.sort method implement

--- function/buildin-method.js
+++ function/buildin-method.js
@@ -29,8 +29,49 @@ console.log("splice item: [" + item + "], anotherArray array item: [" + anotherA
 // 如果Item是一个数组,它会把参数数组作为单个元素添加到数组中,并返回这个array的新长度.
 var length = array.push(anotherArray, true);
 console.log("array push method: [" + array + "], length:" + length);
-
-Array.method('push', function() {
-    this.splice.apply(this, [this.length, 0].concat(Array.prototype.slice.apply(arguments)));
+/*Array.method('push', function() {
+    this.splice.apply(this,
+        // todo:
+        [this.length, 0].concat(Array.prototype.slice.apply(arguments)));
     return this.length;
+});*/
+
+// array.reverse()方法反转array元素的顺序，并返回array本身
+var reverseArray = joinArray.reverse();
+console.log("reverse array: [" + reverseArray + "]");
+
+// array.shift()方法会移除array中第1个元素并返回该元素,如果这个数组为空的则会返回undefined,shift通常比pop慢得多.
+var shiftArray = ["a", "b", "c"];
+var shiftChar = shiftArray.shift();
+console.log("shiftArray.shift method, shiftChar: [" + shiftChar + "], shiftArray: [" + shiftArray + "]");
+
+// slice方法会对array中的一段做浅复制,首先复制array[start],一致复制到array[end]为止.
+var sliceArray = ["a", "b", "c"];
+var b = sliceArray.slice(0, 1);
+var c = sliceArray.slice(1);
+var d = sliceArray.slice(1, 2);
+console.log("position(0,1): [" + b + "], position(1): [" + c + "], position(1, 2): [" + d + "]");
+
+// 使用array.sort(comparefn)对item进行排序,其不能正确地给一组数字排序.
+var arrayItem = [4, 8, 15, 16, 23, 42];
+arrayItem.sort();
+console.log("sort item in array: [" + arrayItem + "]");
+// 使用array的integer元素进行进行排序.
+arrayItem.sort(function (a, b) {
+    return a - b;
 });
+console.log("Use custom array compare: [" + arrayItem + "]");
+// 如果对array中的所有元素进行排序,但其不能使字符串排序. 如果我们想要给任何包含简单值的数组排序,必须要做更多的工作.
+var variousItem = ["aa", "bb", "a", 4, 8, 15, 16, 23, 42];
+variousItem.sort(function(prev, next) {
+    if (prev === next) {
+        return 0;
+    }
+    if (typeof prev === typeof next) {
+        return prev < next ? -1 : 1;
+    }
+    return typeof prev < typeof next ? -1 : 1;
+});
+console.log("various item sort: [" + variousItem + "]");
+
+
